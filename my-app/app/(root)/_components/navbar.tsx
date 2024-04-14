3f9b2e26efdeee28ce315ab71c4993950f6e374a
shuhrat <isoilovshuhrat567@gmail.com>
2024-04-14 16:40:43 +0500

    change first commited

---
+++ my-app/app/(root)/_components/navbar.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Link from "next/link";
+import {navLinks} from "@/constants";
+import {Search} from "lucide-react";
+import ModeToggle from "@/components/shared/mode-toggle";
+
+function Navbar() {
+    return (
+        <div className={"h-[10vh] backdrop-blur-sm border-b fixed z-40 inset-0 bg-background"}>
+            <div className={" container max-w-6xl mx-auto h-[10vh] w-full flex items-center justify-between"}>
+                {/*Logo*/}
+                <Link href={"/"}>
+                    <h1 className={"text-4xl font-creteRound"}>Blog</h1>
+                </Link>
+                {/*	Nav links*/}
+                <div className={'gap-2 hidden md:flex'}>
+                    {navLinks.map(nav => (
+                        <Link
+                            href={nav.route}
+                            key={nav.route}
+                            className={"hover:bg-blue-400/20 py-1 px-3 cursor-pointer rounded-sm transition-colors"}
+                        >
+                            {nav.name}
+                        </Link>
+                    ))}
+                </div>
+                {/*	Search*/}
+                <div className={"gap-1 flex items-center"}>
+                    <div className={"gap-1 hover:bg-blue-400/20 py-1 px-3 cursor-pointer rounded-sm transition-colors flex items-center"}>
+                        <span className={"hidden md:flex"}>Search</span>
+                        <Search/>
+                    </div>
+                </div>
+                <ModeToggle/>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
