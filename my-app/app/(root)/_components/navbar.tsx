cd3672c7ff3377b57bdb9fd929741e024e145b51
shuhrat <isoilovshuhrat567@gmail.com>
2024-04-15 13:18:12 +0500

    second change navbar

--- my-app/app/(root)/_components/navbar.tsx
+++ my-app/app/(root)/_components/navbar.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import Link from "next/link";
 import {navLinks} from "@/constants";
-import {Search} from "lucide-react";
 import ModeToggle from "@/components/shared/mode-toggle";
+import GlobalSearch from "@/app/(root)/_components/global-search";
+import Image from "next/image";
 
 function Navbar() {
     return (
         <div className={"h-[10vh] backdrop-blur-sm border-b fixed z-40 inset-0 bg-background"}>
             <div className={" container max-w-6xl mx-auto h-[10vh] w-full flex items-center justify-between"}>
-                {/*Logo*/}
+                <Image src={"/main-img/Brand.png"} alt={"brand"} height={50} width={50} className={"me-[-180px]"}></Image>
                 <Link href={"/"}>
-                    <h1 className={"text-4xl font-creteRound"}>Blog</h1>
+                    <h1 className={"text-4xl font-creteRound"}>EduPress</h1>
                 </Link>
                 {/*	Nav links*/}
                 <div className={'gap-2 hidden md:flex'}>
@@ -26,12 +27,14 @@ function Navbar() {
                 </div>
                 {/*	Search*/}
                 <div className={"gap-1 flex items-center"}>
+                    <div>Login / Register</div>
                     <div className={"gap-1 hover:bg-blue-400/20 py-1 px-3 cursor-pointer rounded-sm transition-colors flex items-center"}>
-                        <span className={"hidden md:flex"}>Search</span>
-                        <Search/>
+                        <GlobalSearch/>
+
                     </div>
+                    <ModeToggle/>
                 </div>
-                <ModeToggle/>
+
             </div>
         </div>
     )
